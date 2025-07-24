50010e827b58825d4479cb53b1f651fd3fbe4222
Felipe <kleinschmidt.felipe@escola.pr.gov.br>
2025-07-24 19:42:29 -0300

    Add files via upload

---
+++ Felipe Nunes Projeto 2/flashcard-aula_10/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Sobre HTML',
+    'O que acontece se você esquecer de fechar uma tag div em um documento HTML?',
+    ' Pode afetar a estrutura e o layout, mas a página ainda funciona'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual organela é responsável pela produção de energia nas células?',
+    'mitocôndria'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é o passado do verbo "ir"?',
+    'Went'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a molécula que carrega as informações genéticas?',
+    'DNA'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Rússia'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada exata de 144?',
+    '12'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o símbolo químico do elemento oxigênio?',
+    'O'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a função do atributo "alt" na tag "img"?',
+    'Mostra um texto se a imagem não carregar'
+)
